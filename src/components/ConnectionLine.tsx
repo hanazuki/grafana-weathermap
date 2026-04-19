edf1d412a677f411685dc8d1bbab058620fef8c8
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-04-19 13:36:11 +0000

    ConnectionLine: wrap with React.memo

--- src/components/ConnectionLine.tsx
+++ src/components/ConnectionLine.tsx
@@ -1,24 +1,26 @@
 import { useTheme2 } from '@grafana/ui';
 import type { ConnectionLineComponentProps } from '@xyflow/react';
-import type React from 'react';
+import React from 'react';
 
-export const ConnectionLine: React.FC<ConnectionLineComponentProps> = ({ fromNode, toX, toY, connectionLineStyle }) => {
-  const theme = useTheme2();
+export const ConnectionLine = React.memo<ConnectionLineComponentProps>(
+  ({ fromNode, toX, toY, connectionLineStyle }) => {
+    const theme = useTheme2();
 
-  const fromX = fromNode.internals.positionAbsolute.x + (fromNode.width ?? 0) / 2;
-  const fromY = fromNode.internals.positionAbsolute.y + (fromNode.height ?? 0) / 2;
+    const fromX = fromNode.internals.positionAbsolute.x + (fromNode.width ?? 0) / 2;
+    const fromY = fromNode.internals.positionAbsolute.y + (fromNode.height ?? 0) / 2;
 
-  return (
-    <g>
-      <line
-        x1={fromX}
-        y1={fromY}
-        x2={toX}
-        y2={toY}
-        stroke={theme.colors.text.disabled}
-        strokeWidth={connectionLineStyle?.strokeWidth ?? 4}
-        fill="none"
-      />
-    </g>
-  );
-};
+    return (
+      <g>
+        <line
+          x1={fromX}
+          y1={fromY}
+          x2={toX}
+          y2={toY}
+          stroke={theme.colors.text.disabled}
+          strokeWidth={connectionLineStyle?.strokeWidth ?? 4}
+          fill="none"
+        />
+      </g>
+    );
+  },
+);
